dc7bb29f373c4699dc504ffe452eb97c5950ffce
mariusud <marius.udnaes@cognite.com>
2024-04-19 10:01:07 +0200

    create animation, update readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # point-to-plane-ICP
 
-point-to-plane ICP using symforce
+Implementation of point-to-plane Iterative Closest Point (ICP) using symforce.
+
+![Alt Text](assets/animation.gif)
+
+## Problem Description
+
+This project implements the point-to-plane ICP algorithm using the symforce library. The ICP algorithm is commonly used for aligning two point clouds by minimizing the distance between corresponding points and their corresponding point, line or plane correspodences.
+
+### Residual
+
+The residual function used in this implementation computes the difference between the estimated plane and the corresponding points in the point cloud.
+
+## Install
+
+To install the required dependencies, use the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+then you can simply run with
+
+```bash
+python3 generate_fixed.py
+```
--- generate_fixed.py
+++ generate_fixed.py
@@ -4,35 +4,73 @@
 import symforce
 
 symforce.set_epsilon_to_symbol()
-
-# -----------------------------------------------------------------------------
-# Create initial Values
-# -----------------------------------------------------------------------------
 import numpy as np
 import sym
 from symforce import typing as T
 from symforce.values import Values
 from symforce.opt.factor import Factor
 from symforce.opt.optimizer import Optimizer
-
 from symforce.opt.factor import visualize_factors
 from symforce.opt.noise_models import IsotropicNoiseModel
 import open3d as o3d
-
 import symforce.symbolic as sf
 from symforce import logger
 import open3d as o3d
+import time
 
 if symforce.get_symbolic_api() != "symengine":
     logger.warning("The 3D Localization example is very slow on sympy. Use symengine.")
 
 
-def visualize_cube_and_points(points, normals, centroids):
-    # Create Open3D point cloud
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(points)
+def visualize_factor_graph(factors, num_factors_to_visualize):
+    selected_factors = []
+    for i, factor in enumerate(factors):
+        selected_factors.append(factor)
+        if i + 1 == num_factors_to_visualize:
+            break
 
-    # Create Open3D line set for the cube
+    dot_graph = visualize_factors(selected_factors)
+    dot_graph.view()
+    dot_graph.render("factor_graph", format="png", cleanup=True)
+
+
+def visualize(values, values_per_iter, save_image=True):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window("Point-to-plane ICP", width=640, height=480)
+    for i, vals in enumerate(values_per_iter):
+        if i == 0:
+            cube_line_set, normal_line_set = create_cube(
+                values["centroids"], values["normals"]
+            )
+
+            geometry = o3d.geometry.PointCloud()
+            init_points = [
+                values["world_T_lidar"] * point for point in values["points"]
+            ]
+            geometry.points = o3d.utility.Vector3dVector(init_points)
+            vis.add_geometry(geometry)
+            vis.add_geometry(cube_line_set)
+
+        world_T_lidar = vals["world_T_lidar"]
+        print(
+            f"Iteration {i}: t = {world_T_lidar.position()}, R = {world_T_lidar.rotation().to_tangent()}"
+        )
+        if save_image:
+            vis.capture_screen_image("temp_%04d.jpg" % i)
+
+        points = [world_T_lidar * point for point in vals["points"]]
+        geometry.points = o3d.utility.Vector3dVector(points)
+        vis.update_geometry(geometry)
+        vis.poll_events()
+        vis.update_renderer()
+
+        vis.poll_events()
+        vis.update_renderer()
+        time.sleep(0.2)
+    vis.destroy_window()
+
+
+def create_cube(centroids, normals):
     cube_lines = [
         [0, 1],
         [1, 2],
@@ -57,29 +95,24 @@ def visualize_cube_and_points(points, normals, centroids):
         [0.5, 0.5, -0.5],
         [0.5, 0.5, 0.5],
     ]
-    cube_lines_set = []
-    for vertices in cube_lines:
-        cube_lines_set.append([cube_vertices[vertices[0]], cube_vertices[vertices[1]]])
+
+    # Create Open3D line set for the cube
     cube_line_set = o3d.geometry.LineSet()
     cube_line_set.points = o3d.utility.Vector3dVector(cube_vertices)
     cube_line_set.lines = o3d.utility.Vector2iVector(cube_lines)
 
     # Create Open3D lines for the normals
     normal_lines = []
-    for i in range(len(centroids)):
-        normal_lines.append([centroids[i], centroids[i] + normals[i]])
-
+    for centroid, normal in zip(centroids, normals):
+        normal_lines.append([centroid, centroid + normal])
     normal_line_set = o3d.geometry.LineSet()
     normal_line_set.points = o3d.utility.Vector3dVector(
         np.concatenate(normal_lines, axis=0)
     )
-    lines = []
-    for i in range(len(normal_lines)):
-        lines.append([2 * i, 2 * i + 1])
+    lines = [[2 * i, 2 * i + 1] for i in range(len(normal_lines))]
     normal_line_set.lines = o3d.utility.Vector2iVector(lines)
 
-    # Visualize
-    o3d.visualization.draw_geometries([pcd, cube_line_set, normal_line_set])
+    return cube_line_set, normal_line_set
 
 
 def generate_points_on_cube_surface(num_points_per_face):
@@ -227,28 +260,19 @@ def main() -> None:
 
     factors = build_factors(num_points_per_face * NUM_FACES)
 
-    num_factors_to_visualize = 1
-    selected_factors = []
-
-    for i, factor in enumerate(factors):
-        selected_factors.append(factor)
-        if i + 1 == num_factors_to_visualize:
-            break
-
-    # dot_graph = visualize_factors(selected_factors)
-    # dot_graph.view()
-    # dot_graph.render("factor_graph", format="png", cleanup=True)
+    # visualize_factor_graph(factors, num_factors_to_visualize=1)
 
     optimized_keys = [f"world_T_lidar"]
 
     optimizer = Optimizer(
         factors=factors,
         optimized_keys=optimized_keys,
-        # Return problem stats for every iteration
-        debug_stats=True,
         # Customize optimizer behavior
         params=Optimizer.Params(
-            verbose=True, initial_lambda=1e4, lambda_down_factor=1 / 2.0
+            verbose=True,
+            initial_lambda=1e4,
+            lambda_down_factor=1 / 2.0,
+            debug_stats=True,
         ),
     )
 
@@ -258,28 +282,7 @@ def main() -> None:
     values_per_iter = [
         optimizer.load_iteration_values(stats.values) for stats in result.iterations
     ]
-
-    for i, vals in enumerate(values_per_iter):
-        world_T_lidar = vals["world_T_lidar"]
-
-        print(
-            f"Iteration {i}: t = {world_T_lidar.position()}, R = {world_T_lidar.rotation().to_tangent()}"
-        )
-
-        # Rotate the points
-        # rotated_points = [
-        # [world_T_lidar.rotation() * sf.V3(*point) for point in values["points"]]
-        # ]
-
-        # Visualize the rotated points
-
-        points = [world_T_lidar * point for point in vals["points"]]
-        if i % 5 == 0:
-            visualize_cube_and_points(
-                points,
-                vals["normals"],
-                vals["centroids"],
-            )
+    visualize(values, values_per_iter)
 
     # Print some values
     print(f"Num iterations: {len(result.iterations) - 1}")
